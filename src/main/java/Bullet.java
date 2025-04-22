ace3386892b634333b4cd532d04c26756829c9b9
Jared <jaredtponting@gmail.com>
2025-04-23 00:25:29 +0100

    EGG

--- src/main/java/Bullet.java
+++ src/main/java/Bullet.java
@@ -1,10 +1,22 @@
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.Random;
 
 public class Bullet {
+    private Random random = new Random();
     private double x, y;
     private double speed = 10;
     private double dx, dy;
-    private boolean isActive = true;  // Bullet disappears when it's inactive
+    private boolean isActive = true;
+
+    private BufferedImage bulletSpriteSheet;
+    private BufferedImage[] bulletSprites;
+    private BufferedImage bulletSprite;
+
+    private int spriteWidth = 5;
+    private int spriteHeight = 6;
+    private int spriteSize = 32;
+    private int numberEggs = 30;
 
     public Bullet(double x, double y, double targetX, double targetY) {
         this.x = x;
@@ -14,6 +26,19 @@ public class Bullet {
         double angle = Math.atan2(targetY - y, targetX - x);
         dx = Math.cos(angle) * speed;
         dy = Math.sin(angle) * speed;
+
+        bulletSpriteSheet = SpriteLoader.load("/sprites/bulletSpriteSheet.png");
+        bulletSprites = new BufferedImage[30];
+        int i = 0;
+        for (int t = 0; t < spriteWidth; t++) {
+            for (int k = 0; k < spriteHeight; k++) {
+                bulletSprites[i] = bulletSpriteSheet.getSubimage(t * spriteSize, k * spriteSize, spriteSize, spriteSize);
+                i += 1;
+            }
+        }
+        bulletSprite = bulletSprites[random.nextInt(30)];
+
+
     }
 
     public void update() {
@@ -27,8 +52,7 @@ public class Bullet {
     }
 
     public void render(Graphics g) {
-        g.setColor(Color.BLACK);
-        g.fillRect((int) x, (int) y, 5, 5);  // 5x5 pixel square bullet
+        g.drawImage(bulletSprite, (int) x, (int) y, 64, 64, null);
     }
 
     public boolean isActive() {
