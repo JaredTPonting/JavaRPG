46bec32d124fb3d42e7e02d30a6e1e5c2575fb5c
Jared <jaredtponting@gmail.com>
2025-05-19 14:03:47 +0100

    clamp the camera

--- src/main/java/utils/Camera.java
+++ src/main/java/utils/Camera.java
@@ -5,9 +5,9 @@ import player.Player;
 class Camera {
     int camX, camY;
 
-    public void centerOn(Player player, int screenWidth, int screenHeight) {
-        camX = player.getX() - screenWidth / 2;
-        camY = player.getY() - screenHeight / 2;
+    public void centerOn(Player player, int screenWidth, int screenHeight, int mapWidth, int mapHeight) {
+        camX = Math.max(0, Math.min(player.getX() - screenWidth / 2, mapWidth - screenWidth));
+        camY = Math.max(0, Math.min(player.getY() - screenHeight / 2, mapHeight - screenHeight));
     }
 
     public int getX() { return camX; }
