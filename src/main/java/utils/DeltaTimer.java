0400f33254651b27c7bf50a2dbfb857cd6c4484a
Jared <jaredtponting@gmail.com>
2025-10-23 19:47:17 +0100

    she loves delta BAD

--- src/main/java/utils/DeltaTimer.java
+++ src/main/java/utils/DeltaTimer.java
@@ -2,18 +2,33 @@ package utils;
 
 public class DeltaTimer {
     private long lastTime;
+    private boolean paused;
 
     public DeltaTimer() {
-        this.lastTime = System.nanoTime();
+        lastTime = System.nanoTime();
     }
+
     public double getDelta() {
+        if (paused) {
+            return 0;
+        }
         long now = System.nanoTime();
         double delta = (now - lastTime) / 1_000_000_000.0;
         lastTime = now;
         return delta;
     }
 
-    public void reset() {
-        lastTime = System.nanoTime();
+    public void pause() {
+        if (!paused) {
+            paused = true;
+        }
+    }
+
+    public void resume() {
+        if (paused) {
+            paused = false;
+            lastTime = System.nanoTime(); // reset so delta after resume is small
+        }
     }
 }
+
