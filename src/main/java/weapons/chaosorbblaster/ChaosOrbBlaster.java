f5dba5e8ddcdbc4dc3d7a776cab7c4fdc33a5ba9
Jared <jaredtponting@gmail.com>
2025-10-23 19:46:13 +0100

    update for new chaos orbs

--- src/main/java/weapons/chaosorbblaster/ChaosOrbBlaster.java
+++ src/main/java/weapons/chaosorbblaster/ChaosOrbBlaster.java
@@ -5,6 +5,7 @@ import projectiles.Projectile;
 import projectiles.chaosorb.ChaosOrb;
 import core.GameWorld;
 import weapons.Weapon;
+import weapons.WeaponMods;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,22 +13,17 @@ import java.util.Random;
 
 public class ChaosOrbBlaster extends Weapon {
 
-    private Random random = new Random();
-
     public ChaosOrbBlaster(Entity owner, long cooldownMillis, GameWorld gameWorld) {
         super(owner, cooldownMillis, gameWorld);
     }
 
     @Override
     protected List<Projectile> createProjectiles() {
-        List<Projectile> projectiles = new ArrayList<>();
-
-        // random direction unit vector
-        double angle = random.nextDouble() * 2 * Math.PI;
-        double dx = Math.cos(angle);
-        double dy = Math.sin(angle);
-
-        projectiles.add(new ChaosOrb(gameWorld, owner.getX(), owner.getY(), dx, dy));
-        return projectiles;
+        List<Projectile> newOrbs = new ArrayList<>();
+        newOrbs.add(new ChaosOrb(gameWorld));
+        for (WeaponMods mod : this.weaponMods) {
+            mod.addProjectile(this.gameWorld, newOrbs);
+        }
+        return newOrbs;
     }
 }
